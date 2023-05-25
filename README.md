d839c1d0afafa1ab983432d4be5dfa377118c076
Luis Miguel Pires <luismiguelprs@gmail.com>
2023-05-25 15:13:53 -0300

    v0.0.5

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # serverless-offline-dynamodb
+
+! This plugin is under development. Feel free to use it and collaborate!
+
+## How to use:
+
+1) Start a container of amazon/dynamodb-local:
+
+docker-compose.yml example:
+
+```
+version: '3'
+services:
+  dynamodb:
+    container_name: logistics-api-offline-dynamodb
+    image: amazon/dynamodb-local
+    ports:
+      - '8000:8000'
+    working_dir: /home/dynamodblocal
+    command: '-jar DynamoDBLocal.jar -sharedDb -dbPath ./'
+```
+
+2) Add 'serverless-offline-dynamodb' in plugins section of serverless (be sure to add ir BEFORE serverless-offline) 
+
+3) The Plugin will create all tables
+
+ - Tip: You can run `npx dynamodb-admin -H localhost` to check your database 
+
+## How to Seed data:
+
+To seed data, just create a folder called `seed` on project root and put a json file with exact same name of table you want to seed.
---
+++ src/index.ts
@@ -0,0 +1,98 @@
+import fs from 'fs'
+import path from 'path'
+
+import {
+  CreateTableCommand,
+  DynamoDBClient,
+  ListTablesCommand,
+  PutItemCommand,
+} from '@aws-sdk/client-dynamodb'
+import { marshall } from '@aws-sdk/util-dynamodb'
+import Serverless from 'serverless'
+
+class OfflineDynamoDB {
+
+  serverless: Serverless
+  dynamoDBClient: DynamoDBClient
+  hooks: { [key: string]: () => Promise<void> }
+
+  constructor(serverless: Serverless) {
+    this.serverless = serverless
+
+    console.log('Initializing Offline DynamoDB Plugin...')
+
+    this.dynamoDBClient = new DynamoDBClient({
+      endpoint: 'http://localhost:8000',
+      credentials: {
+        accessKeyId: 'DEFAULT_ACCESS_KEY',
+        secretAccessKey: 'DEFAULT_SECRET',
+      },
+    })
+
+    this.hooks = {
+      'offline:start': () => this.migrate(),
+    }
+  }
+
+  get tablesToCreate() {
+    const resources = this.serverless.service.resources.Resources ?? []
+    const tableTypes = ['AWS::DynamoDB::Table', 'AWS::DynamoDB::GlobalTable']
+    const tables = []
+    for (const key in resources) {
+      const resource = resources[key]
+      if (tableTypes.includes(resource.Type)) {
+        if (resource.Properties.BillingMode === 'PAY_PER_REQUEST') {
+          delete resource.Properties.BillingMode
+
+          const defaultProvisioning = {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5,
+          }
+          resource.Properties.ProvisionedThroughput = defaultProvisioning
+        }
+
+        delete resource.Properties.Replicas
+
+        tables.push(resource.Properties)
+      }
+    }
+    return tables
+  }
+
+  async migrate() {
+    const listTablesCommand = new ListTablesCommand({})
+    const { TableNames: existingTableNames } = await this.dynamoDBClient.send(
+      listTablesCommand,
+    )
+
+    for (const table of this.tablesToCreate) {
+      try {
+        if (!existingTableNames?.includes(table.TableName)) {
+          console.log('Creating Table:', table.TableName)
+          const createGlobalTableCommand = new CreateTableCommand(table)
+          await this.dynamoDBClient.send(createGlobalTableCommand)
+          await this.seed(table.TableName)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+
+  async seed(tableName: string) {
+    const filePath = path.join(process.cwd(), `/seed/${tableName}.json`)
+    if (fs.existsSync(filePath)) {
+      const seedData = require(filePath)
+      for (const data of seedData) {
+        console.log('Seeding Table:', tableName, data)
+        const putItemCommand = new PutItemCommand({
+          TableName: tableName,
+          Item: marshall(data),
+        })
+        await this.dynamoDBClient.send(putItemCommand)
+      }
+    }
+  }
+}
+
+export = OfflineDynamoDB
